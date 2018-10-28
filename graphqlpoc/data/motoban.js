111bda13c4d14771b32d6f097bd4609ad6d27472
shan_alexander_newton <shan.newton@gmail.com>
2018-10-28 21:54:26 +0700

    adds db spreadsheet and new schemas

--- graphqlpoc/data/motoban.js
+++ graphqlpoc/data/motoban.js
@@ -18,11 +18,37 @@ let UserType = new GraphQLObjectType({
     user_id: { type: GraphQLID },
     username: { type: GraphQLString },
     password: { type: GraphQLString },
+    bikes: { type: GraphQLList(BikeType)}
   })
 });
 
+
+let BikeType = new GraphQLObjectType({
+  name: "bike",
+  fields: () => ({
+    bike_id: { type: GraphQLID },
+    users_id_fkey: { type: GraphQLInt },
+    bikes_id_fkey: { type: GraphQLInt },
+    maker: { type: GraphQLString },
+    model: { type: GraphQLString },
+    year: { type: GraphQLInt },
+    description: { type: GraphQLString },
+    condition: { type: GraphQLString }
+  })
+});
+
+// let UserWithAllBikesType = new GraphQLObjectType({
+//   name: "userWithAllBikes",
+//   fields: () => ({
+//     user_id: { type: GraphQLID },
+//     username: { type: GraphQLString },
+//     password: { type: GraphQLString },
+//     bikes: { type: GraphQLList(BikeType) }
+//   })
+// });
+
 const RootQuery = new GraphQLObjectType({
-  name: "RootQueryType",
+  name: "MotobanSchema",
   fields: {
     user: {
       type: UserType,
@@ -43,6 +69,60 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
         const query = `SELECT * FROM public."users"`;
+        return db.conn
+          .any(query)
+          .then(data => {
+            return data;
+          })
+          .catch(err => {
+            return "The error is", err;
+          });
+      }
+    },
+    bike: {
+        type: BikeType,
+        args: { bike_id: { type: GraphQLID } },
+        resolve(parentValue, args) {
+            const query = `SELECT * FROM public."bikes" b inner join public."bikes_descriptions" bd on b.bike_id = bd.bikes_id_fkey WHERE bike_id=${args.bike_id} `;
+            return db.conn
+            .one(query)
+            .then(data => {
+                return data;
+            })
+            .catch(err => {
+                return "The error is", err;
+            });
+        }
+    },
+    bikes: {
+        type: new GraphQLList(BikeType),
+        args: { id: { type: GraphQLID } },
+        resolve(parentValue, args) {
+            const query = `SELECT * FROM public."bikes" b 
+                            inner join 
+                        public."bikes_descriptions" bd 
+                            on 
+                        b.bike_id = bd.bikes_id_fkey`;
+            return db.conn
+            .any(query)
+            .then(data => {
+                return data;
+            })
+            .catch(err => {
+                return "The error is", err;
+            });
+        }
+    },
+    bikesOfUser: {
+      type: new GraphQLList(BikeType),
+      args: { user_id: { type: GraphQLID } },
+      resolve(parentValue, args) {
+        const query = `SELECT * FROM 
+                        public."bikes" b
+                          inner join
+                        public."bikes_descriptions" bd
+                          on b.bike_id = bd.bikes_id_fkey
+                        WHERE b.users_id_fkey=${args.user_id}`;
         return db.conn
           .any(query)
           .then(data => {
